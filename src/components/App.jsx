45253ee7979cf7ae1fc8c7c1f1cc487086731972
Olena Savchenko <elena.volobuieva@gmail.com>
2023-04-22 10:54:34 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Homework #2
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackBox } from './App.styled';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = event => {
+    // console.log(c);
+    // const targetValue = event.target;
+    this.setState(prevState => {
+      console.log('Значення prevState: ', prevState);
+      console.log('event.target.name: ', [event.target]);
+      console.log(
+        'prevState[event.target.] + 1: ',
+        prevState[event.target] + 1
+      );
+      return { [event.target]: prevState[event.target] + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <FeedbackBox>
+        <p>Please leave feedback</p>
+        <button
+          type="button"
+          name={this.state.good}
+          onClick={this.handleIncrement}
+        >
+          Good
+        </button>
+        <button
+          type="button"
+          name={this.state.neutral}
+          onClick={this.handleIncrement}
+        >
+          Neutral
+        </button>
+        <button
+          type="button"
+          name={this.state.bad}
+          onClick={this.handleIncrement}
+        >
+          Bad
+        </button>
+        <p>Statistics</p>
+        <div>
+          <span>Good: {this.state.good}</span>
+          <span>Neutral: {this.state.neutral}</span>
+          <span>Bad: {this.state.bad}</span>
+        </div>
+      </FeedbackBox>
+    );
+  }
+}
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const FeedbackBox = styled.div`
+  width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+`;
